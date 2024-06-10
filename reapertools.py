3f4287a4adeb00c06ba0bb2d5b7743202ad6e62d
zachjvandyke <zachjvandyke@gmail.com>
2024-06-10 16:03:34 -0500

    added making StereoMix a folder with a muted item and the sub tracks all the tracks that make up the mix

--- reapertools.py
+++ reapertools.py
@@ -11,7 +11,11 @@ def extract_text(file_path):
 
 
 def build_item(wav_path, position, duration, soffset, title=""):
-    rpp_out = (f"\n\t\t<ITEM\n\t\t\tPOSITION {position}\n\t\t\tLENGTH {str(duration)}\n\t\t\t"
+    if title == 'StereoMix':
+        mute = 'MUTE 1 0'
+    else:
+        mute = 'MUTE 0 0'
+    rpp_out = (f"\n\t\t<ITEM\n\t\t\tPOSITION {position}\n\t\t\tLENGTH {str(duration)}\n\t\t\t{mute}\n\t\t\t"
                f"SOFFS {str(soffset)}\n\t\t\tNAME \"{title}\"\n\t\t\t<SOURCE WAVE\n\t\t\t\t"
                f"FILE '{wav_path}'\n\t\t\t>\n\t\t>")
     return rpp_out
@@ -19,10 +23,16 @@ def build_item(wav_path, position, duration, soffset, title=""):
 
 def build_rpp(name, wav_list, out_path):
     tracks = []
-    for wav_path in wav_list:
-        track_name = extract_text(wav_path)
+    for idx, wav_path in enumerate(wav_list):
+        tn = extract_text(wav_path)
+        if tn == 'StereoMix':
+            track_name = f'{tn}\n\t\tISBUS 1 1'
+        elif idx == len(wav_list)-1:
+            track_name = f'{tn}\n\t\tISBUS 2 -1'
+        else:
+            track_name = f'{tn}\n\t\tISBUS 0 0'
         duration = librosa.get_duration(path=wav_path)
-        item = build_item(wav_path, 0, duration, 0, '')
+        item = build_item(wav_path, 0, duration, 0, tn)
         tracks.append(f"\n\t<TRACK\n\t\tNAME {track_name}{item}\n\t>")
 
     start = "<REAPER_PROJECT\n"
